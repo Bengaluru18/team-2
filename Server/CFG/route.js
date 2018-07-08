236103360239c82e12f696f23734c9f0ef9c0afa
beheraamrita <amritabehera2015@gmail.com>
2018-07-08 05:32:49 +0530

    update email

--- Server/CFG/route.js
+++ Server/CFG/route.js
@@ -3,12 +3,22 @@ var bodyParser = require('body-parser');
 var app = express();  
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 // Connect to the db
+var nodemailer = require('nodemailer'); 
 
 var mysql = require('mysql');
 
+/*
+const accountSid = 'AC683dcb728d1c27780ca9cff532730ea3';
+const authToken = '413053016fd2448ad74eb1a3dd383a56';
+const client = require('twilio')(accountSid, authToken);
+*/
+
+
+
+ 
 var con = mysql.createConnection({
-host: "13.229.237.24",
-//host: "localhost",
+//host: "13.229.237.24",
+host: "localhost",
   user: "root",
   password: "root1234",
 database: "ISAP"
@@ -24,15 +34,69 @@ app.use(bodyParser.json());
 
 app.get('/', function (req, res) {  
 console.log("First Page");
-   res.sendFile( __dirname + "/" + "index.html" );    
+ //  res.sendFile( __dirname + "/" + "index.html" );    
 })  
 
 
 //LOGIN STUFF 
 
+app.get('/send', function (req, res) {  
+console.log("sending  text ... ");
+/*
+client.messages
+      .create({
+         body: 'Lets grabch at Milliways tomorrow!',
+         from: '+18325398167',
+         //mediaUrl: 'http://www.example.com/cheeseburger.png',
+         to: '+917022495226'
+       })
+      .then(message => console.log(message.sid + " "))
+      .done();
+*/
 
 
 
+//var query = "SELECT date_add(CURDATE(), INTERVAL 3 DAY)"
+
+
+con.query("SELECT f.fname, b.returndate FROM Farmer f, booking b where b.returndate= date_add(CURDATE(),INTERVAL 3 DAY)", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+	//res.send(result);
+
+//var obj= JSON.parse(result);
+//console.log(obj.fname);
+//console.log(obj.date);
+
+var transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'cfgtest2018@gmail.com', 
+    pass: 'root1234$'
+  }
+});
+
+var mailOptions = {
+  from: 'cfgtest2018@gmail.com',
+  to: 'cfgtest2018@gmail.com',
+  subject: 'Sending Email using Node.js....',
+  text: 'Hi' +result.fname +'The product return date is' +result.date
+};
+
+transporter.sendMail(mailOptions, function(error, info){
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
+}); 
+
+
+  });
+
+
+
+})  
 
 
 app.get('/dashboard', function (req, res) {  
@@ -43,28 +107,45 @@ console.log("hello");
 
 
 
-app.get('/getequip/:cid', function (req, res) {  
-//var cid = 11; //req.query.cid;	
+app.get('/getinfo', function (req, res) {  
+console.log("......");
+  con.query("SELECT * FROM product", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
 
+
+}) 
+
+app.get('/getequip/:cid', function (req, res) {  
+//var cid = 11; //req.query.cid;  
+console.log("jsn");
 con.query("SELECT * FROM product where cid =" + req.params.cid, function (err, result, fields) {
     if (err) throw err;
     console.log(result);
-	res.send(result);
+  res.send(result);
   });
 
 }); 
 
-app.get('/app/getequip', function (req, res) {  
-var cid = 11; //req.query.cid;	
 
-con.query("SELECT * FROM center", function (err, result, fields) {
+app.get('/farmer/:fid', function (req, res) {  
+con.query("SELECT * FROM Farmer where fid=" +req.params.fid, function (err, result, fields) {
     if (err) throw err;
     console.log(result);
-	res.send(result);
+  res.send(result);
   });
 
 }); 
+ 
+app.get('/booking/:fid', function (req, res) {  
+con.query("SELECT * FROM booking where fid=" +req.params.fid, function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  res.send(result);
+  });
 
+}); 
 
 
 
